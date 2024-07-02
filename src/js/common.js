2b67ac30acd7a65ce35cfeabb92d2c2a95f7eef7
Gyuri-v <fnffkdll@naver.com>
2024-07-03 00:28:52 +0900

    setting update

--- src/js/common.js
+++
@@ -1,6 +0,0 @@
-export const getPoint = function (e) {
-  if (e.touches) {
-    e = e.touches[0] || e.changedTouches[0];
-  }
-  return [e.pageX || e.clientX, e.pageY || e.clientY];
-};
--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,73 @@
+import '../scss/style.scss'
+
+import gsap from 'gsap';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/addons/controls/OrbitControls';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
-const DEBUG = location.search.indexOf('debug') > -1;
+import { commonSetting } from './_common';
 
+// common setting
+commonSetting();
+
+// app
 const App = function () {
-  let ww, wh;
-  let renderer, scene, camera, light, controls;
-  let isRequestRender = false;
-
+  // * DOM
   const $container = document.querySelector('.container');
   let $canvas;
 
-  const init = function () {
-    // Window
-    ww = window.innerWidth;
-    wh = window.innerHeight;
+  // * VALUE
+	let areaWidth = window.innerWidth;
+	let areaHeight = window.innerHeight;
+  let isRequestRender = false;
+
+  // * WORLD
+  let renderer, scene, camera, controls;
+	let model, modelSize, modelHeight, modelDepth, modelWidth;
+
+  // ### INIT
+  const onInit = function () {
+    onResize();
 
     // Scene
     scene = new THREE.Scene();
 
     // Renderer
-    renderer = new THREE.WebGL1Renderer({ antialias: true, alpha: true });
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     renderer.setClearColor('#000', 1.0);
-    renderer.setSize(ww, wh);
+    renderer.setSize(areaWidth, areaHeight);
     $canvas = renderer.domElement;
     $container.appendChild($canvas);
 
     // Camera
-    camera = new THREE.PerspectiveCamera(70, ww / wh, 1, 999);
-    camera.position.set(0, 0, 10);
+    camera = new THREE.PerspectiveCamera(70, areaWidth / areaHeight, 1, 999);
+    camera.position.set(1, 1, 3);
+    camera.lookAt(0, 0, 0)
     scene.add(camera);
 
     // Light
-    light = new THREE.AmbientLight('#fff', 1);
-    scene.add(light);
+    const ambientLight = new THREE.AmbientLight('#fff', 1);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(0, 5, 5);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.width = 512;
+    directionalLight.shadow.mapSize.height = 512;
+    directionalLight.shadow.camera.near = 0.5;
+    directionalLight.shadow.camera.far = 50;
+    directionalLight.shadow.camera.left = -25;
+    directionalLight.shadow.camera.right = 25;
+    directionalLight.shadow.camera.top = 20;
+    directionalLight.shadow.camera.bottom = -20;
+    directionalLight.shadow.radius = 1;
+    directionalLight.shadow.bias = -0.003;
+
+    scene.add(ambientLight, directionalLight);
 
     // Controls
-    if (DEBUG) {
+    if (_DEBUG) {
       controls = new OrbitControls(camera, $canvas);
       controls.addEventListener('change', renderRequest);
     }
@@ -47,39 +77,60 @@ const App = function () {
 
     // Render
     renderRequest();
-    render();
+    gsap.ticker.add(animate);
 
     // Loading
     THREE.DefaultLoadingManager.onProgress = function (url, itemsLoaded, itemsTotal) {
       if (itemsLoaded === itemsTotal) {
-        render();
       }
     };
   };
 
-  const resize = function () {
-    //
-  };
-
-  // Setting -------------------
+  // ### SETTING
   const setModels = function () {
-    //
+    const mesh = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshStandardMaterial({ color: 'red' })
+    );
+    scene.add(mesh);
   };
 
-  // Render -------------------
+  // ### RENDER
   const renderRequest = function () {
     isRequestRender = true;
   };
-
-  const render = function () {
+  const animate = function () {
     if (isRequestRender) {
-      renderer.setSize(ww, wh);
+      controls && controls.update();
+
       renderer.render(scene, camera);
+
+      isRequestRender = false;
     }
-    window.requestAnimationFrame(render);
   };
 
-  window.addEventListener('load', init);
-  window.addEventListener('resize', resize);
+  // ### RESIZE
+  const onResize = function () {
+    // common
+		areaWidth = $container.offsetWidth;
+		areaHeight = $container.offsetHeight;
+
+    // world
+    if ( camera ) {
+      camera.aspect = areaWidth / areaHeight;
+      camera.updateProjectionMatrix();
+    }
+    if ( renderer ) {
+      const pixelRatio = Math.min(2, window.devicePixelRatio);
+      renderer.setSize(areaWidth, areaHeight);
+      renderer.setPixelRatio(pixelRatio);
+    }
+
+    renderRequest();
+  };
+
+  // ### EVENT
+  window.addEventListener('load', onInit);
+  window.addEventListener('resize', onResize);
 };
 App();
